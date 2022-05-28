7eed5b5b2bd0310f5361e9e3823baad6cb6a0d93
Tanveer Bassi <tanveer13a551@gmail.com>
2022-05-28 10:09:22 +0100

    JS search history

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+const readFromLocalStorage = (key, defaultValue) => {
+  // get from LS using key name
+  const dataFromLS = localStorage.getItem(key);
+
+  // parse data from LS
+  const parsedData = JSON.parse(dataFromLS);
+
+  if (parsedData) {
+    return parsedData;
+  } else {
+    return defaultValue;
+  }
+};
+
+const onReady = () => {
+  const recentSearchesContainer = $("recent-searches-container");
+
+  //get recent searches from LS
+  const recentSearches = readFromLocalStorage("recentSearches", []);
+
+  //if empty show alert
+  if (recentSearches.length) {
+  } else {
+    const alert = `<div class="alert alert-warning" role="alert">
+        A simple warning alert—check it out!
+        </div>`;
+
+    recentSearchesContainer.append(alert);
+  }
+};
+
+$(document).ready(onReady);
